98ce79706f08b1c6ccca92ff8c4d8454e6370d66
Adi Ron <adi@adiron.me>
2016-12-03 13:54:33 +0200

    Added my own keymap. Untested as of now (oooh)

---
+++ keyboard/tv44/keymap_adi.c
@@ -0,0 +1,59 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qerty */
+    KEYMAP(GRV,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   BSPC, \
+           TAB,   A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT, \
+           LSFT,   Z,   X,   C,   V,   B,   N,   M,   COMM,DOT,SLSH,RSHIFT, \
+           LCTL,  LALT,  LGUI, SPC,      ENTER,      RGUI,  FN1, FN2, FN2),
+
+    /* 1: FN 1  - functions and extras layer. */
+    KEYMAP(F1,  F2, F3, F4, F5, F6, F7, F8, F9, F10, F11,   F12, \
+           TRNS, TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,MPRV,  MPLY,MNXT,TRNS,  TRNS, \
+           TRNS, FN5,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,VOLD,VOLU,BSLASH, RSFT, \
+           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS, FN0),
+
+    /* 2: FN 2  - Mousekeys!!! */
+   KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,BTN1, BTN2,TRNS,TRNS, TRNS, \
+          TRNS,TRNS,TRNS,TRNS,MS_WH_UP,MS_WH_DOWN,TRNS,  MS_DOWN,  MS_UP,  MS_RIGHT,  TRNS,  TRNS, \
+          TRNS,    TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,  TRNS, TRNS, TRNS, TRNS,TRNS, \
+          TRNS, TRNS,     TRNS,  TRNS,       TRNS,      TRNS, TRNS, TRNS, TRNS),
+
+    /* 3: FN 3 - Spacebar modifier layer thing  */
+    KEYMAP(ESC, 1,2,3,4,5,6,7,  8,  9,  0,  DELETE, \
+           CAPS, MINUS,EQUAL,LBRACKET,RBRACKET,TRNS,LEFT,  DOWN,  UP,  RIGHT,  TRNS,  TRNS, \
+           TRNS,    TRNS,   TRNS,   FN5,   TRNS,   TRNS,   END,  PGDOWN, PGUP, HOME, BSLASH,TRNS, \
+           TRNS, TRNS,     TRNS,  TRNS,       FN4,      TRNS, TRNS, TRNS, TRNS),
+
+   /* 4:  LED Mode */
+   KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS, \
+          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS, \
+          TRNS,    TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,  TRNS, TRNS, TRNS, TRNS,TRNS, \
+          TRNS, TRNS,     TRNS,  TRNS,       TRNS,      TRNS, TRNS, TRNS, TRNS),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [0]  = ACTION_LAYER_TOGGLE(1), // L1 lock
+    [1]  = ACTION_LAYER_MOMENTARY(1), // L1 normal
+    [2]  = ACTION_LAYER_TOGGLE(2), //  L2 lock
+    [3]  = ACTION_LAYER_TAP_KEY(3, KC_SPC), // L3 space
+    [4]  = ACTION_LAYER_TOGGLE(3), // L3 lock
+    [5]  = ACTION_BACKLIGHT_TOGGLE(), // Backlight toggle
+    [6]  = ACTION_MODS_KEY(MOD_LSFT, KC_3),
+    [7]  = ACTION_MODS_KEY(MOD_LSFT, KC_4),
+    [8]  = ACTION_MODS_KEY(MOD_LSFT, KC_5),
+    [9]  = ACTION_MODS_KEY(MOD_LSFT, KC_6),
+    [10] = ACTION_MODS_KEY(MOD_LSFT, KC_7),
+    [11] = ACTION_MODS_KEY(MOD_LSFT, KC_8),
+    [12] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
+    [13] = ACTION_MODS_KEY(MOD_LSFT, KC_0),
+    [14] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
+    [15] = ACTION_MODS_KEY(MOD_LSFT, KC_QUOT),
+    [16] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),
+    [17] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL),
+    [18] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),
+    [19] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),
+    [20] = ACTION_LAYER_TOGGLE(4),
+    [21] = ACTION_LAYER_TAP_KEY(1, KC_QUOT),
+    [22] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_ESC)
+};
