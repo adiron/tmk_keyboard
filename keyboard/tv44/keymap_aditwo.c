4b6acc4d062f6cdfd2dc14869092de0184d14e80
Adi Ron <adi@adiron.me>
2016-12-25 22:42:48 +0200

    Merged a change from evangs's branch
    This caused some problem with the KEYMAP macro.

--- keyboard/tv44/keymap_aditwo.c
+++ keyboard/tv44/keymap_aditwo.c
@@ -9,35 +9,35 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(GRV,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   BSPC, \
            TAB,   A,   S,   D,   F,   G,   H,   J,   K,   L,  FN10,QUOT, \
            LSFT,   Z,   X,   C,   V,   B,   N,   M,  COMM,DOT,SLSH,RSFT, \
-           LCTL,  LALT,  LGUI, SPC,    FN0,    FN1, RGUI,  RGUI, FN2),
+           LCTL,  LALT,  LGUI, SPC,    FN0,    FN1, RGUI, FN2),
 
     /* 1: Raise layer */
     KEYMAP(ESC,  1, 2, 3, 4, 5, 6,7,8,9,0,  DEL, \
            CAPS, F1,F2,F3, F4,F5,F6, MINS,EQL,LBRC,RBRC,  BSLS, \
            TRNS, F7,F8,F9, F10,F11,F12,NUHS,NUBS,PGUP,PGDN, TRNS, \
-           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS, FN3),
+           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, FN3),
 
     /* 2: ENTFN layer */
     KEYMAP(ESC,  FN6, FN7, TRNS, TRNS, TRNS, TRNS,TRNS,TRNS,FN8,FN9,  DEL, \
            TRNS, TRNS, MS_WH_LEFT, MS_WH_UP, MS_WH_DOWN, MS_WH_RIGHT, LEFT, DOWN, UP, RIGHT, TRNS,  TRNS, \
            TRNS, FN4,FN5,TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS, \
-           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS, FN3),
+           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, FN3),
 
     /* 3: Utils */
     KEYMAP(TRNS,  TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   FN11,   FN12,   TRNS, \
            TRNS,   VOLU,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, MEDIA_REWIND, MPLY, MEDIA_FAST_FORWARD, TRNS, TRNS, \
            TRNS,   VOLD,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, TRNS, TRNS,TRNS,TRNS,TRNS, \
-           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS, TRNS),
+           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS),
     /* 4: Mousekeys */
     KEYMAP(TRNS,  TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   BTN1,   BTN2,   TRNS,   TRNS,   TRNS,   TRNS, \
            TRNS,  TRNS, MS_WH_LEFT, MS_WH_UP ,MS_WH_DOWN, MS_WH_RIGHT, MS_L, MS_D, MS_U, MS_R, TRNS,TRNS, \
            TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, TRNS, TRNS,TRNS,TRNS,TRNS, \
-           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS, TRNS),
+           TRNS, TRNS,  TRNS, TRNS,    TRNS,     TRNS, TRNS, TRNS),
     /* 5: Game layer */
     KEYMAP(GRV,  Q,   W,   E,   R,   T,   Y,   7,   8,   9,   0,   BSPC, \
            TAB,   A,   S,   D,   F,   G,   H,   4,   5,   6,  FN10,QUOT, \
            LSFT,   Z,   X,   C,   V,   B,   N,   1,  2, 3,SLSH,RSFT, \
-           LCTL,  LALT,  LGUI, SPC,    FN0,    FN1, RGUI, RGUI, FN2),
+           LCTL,  LALT,  LGUI, SPC,    FN0,    FN1, RGUI, FN2),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
